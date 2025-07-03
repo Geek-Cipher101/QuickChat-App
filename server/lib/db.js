d80339731a0cf8398c3db752bb0656f58e7f57b3
Geek-Cipher101 <akshatnigam20@outlook.com>
2025-07-03 19:24:05 +0530

    .

---
+++ server/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+// Function to connect to the mongodb database
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.log(error);
+    }
+};
