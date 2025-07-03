a8fb104338faaed6c06499e56d3fe139eb5eb647
Geek-Cipher101 <akshatnigam20@outlook.com>
2025-07-04 00:49:05 +0530

    .

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import cors from 'cors';
-import http from 'http';
-import { connectDB } from './lib/db.js';
-import userRouter from './routes/userRoutes.js';
-import messageRouter from './routes/messageRoutes.js';
-import {Server} from 'socket.io';
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 // Create Express app and HTTP server
 const app = express();
@@ -15,29 +15,28 @@ export const io = new Server(server, {
     cors: {
         origin: "*",
     },
-})
+});
 
 // store online user
 export const userSocketMap = {}; // {userId: socketId}
 // socket.io connection handler
 io.on("connection", (socket) => {
-    
     const userId = socket.handshake.query.userId;
     console.log("User connected: " + userId);
-    
-    if(userId) {
+
+    if (userId) {
         userSocketMap[userId] = socket.id;
     }
 
     //emit online users to all connected clients
     io.emit("getOnlineUser", Object.keys(userSocketMap));
 
-    socket.on("disconnect", () => { 
+    socket.on("disconnect", () => {
         console.log("User disconnected: " + userId);
         delete userSocketMap[userId];
         io.emit("getOnlineUser", Object.keys(userSocketMap));
-    })
-})
+    });
+});
 
 // Middleware setup
 app.use(express.json({ limit: "4mb" }));
@@ -45,14 +44,19 @@ app.use(cors());
 
 // route setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
-app.use("/api/auth", userRouter)
-app.use("/api/messages", messageRouter)
-
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 //Connect to MongoDb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log("Server is running on port: " + PORT)
-);
+    server.listen(PORT, () =>
+        console.log("Server is running on port: " + PORT)
+    );
+}
+
+// export server for vercel
+export default server;
